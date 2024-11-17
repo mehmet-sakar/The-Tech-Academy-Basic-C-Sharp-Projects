51b7939465487e9e05d2b416ef06cb7e9f6d0d24
mehmet-sakar <aolacademy1@outlook.com>
2024-11-17 15:43:25 +0300

    MethodApp
    MethodApp

---
+++ MethodApp/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodApp
+{
+    public class MathOperations
+    {
+        // Create a void method that takes two integers as parameters
+        public void PerformOperation(int num1, int num2)
+        {
+            // Perform a simple math operation on the first integer The number is raised to the second power.
+            var result = Math.Pow(num1, 2);
+
+            // Display the result of the math operation
+            Console.WriteLine(@"The number is raised to the second power.");
+            Console.WriteLine($"The result of the math operation on the first number is: {result}");
+
+            // Display the second integer
+            Console.WriteLine($"The second number is: {num2}");
+        }
+    }
+}
